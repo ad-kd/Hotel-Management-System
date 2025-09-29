ef005ef663ac0e8dc868e8537d442be307e2d4f4
Adhithya K <12218adhithyaksnm@gmail.com>
2025-09-29 18:31:46 +0530

    Init

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/home.jsx'
+
+
+const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes('owner'); 
+
+  return (
+    <div>
+      {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path='/'element={<Home />}/>
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../components/Hero.jsx'
+
+const home = () => {
+  return (
+    <>
+        <Hero />
+    </>
+  )
+}
+
+export default home
