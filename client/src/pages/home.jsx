5f7c9e68e369642acf5350ead14b7c267c242c26
Adhithya K <12218adhithyaksnm@gmail.com>
2025-09-30 19:17:09 +0530

    update home and add new file

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero.jsx'
 import FeaturedDestination from '../components/FeaturedDestination.jsx'
+import ExclusiveOffer from '../components/ExclusiveOffer.jsx'
 
 const home = () => {
   return (
     <>
         <Hero />
         <FeaturedDestination />
+        <ExclusiveOffer />
     </>
   )
 }
