91cfca83fa0be01915f06bd04738cef31b1e41aa
Adhithya K <12218adhithyaksnm@gmail.com>
2025-09-30 18:35:22 +0530

    update hero and add title,card

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero.jsx'
+import FeaturedDestination from '../components/FeaturedDestination.jsx'
 
 const home = () => {
   return (
     <>
         <Hero />
+        <FeaturedDestination />
     </>
   )
 }
