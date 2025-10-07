2a39f072ef8fc188e42e0dff5b3f7a4385e542ba
Adhithya K <12218adhithyaksnm@gmail.com>
2025-10-07 17:26:18 +0530

    update RoomDetail

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/home.jsx'
 import Footer from './components/Footer.jsx'
 import AllRooms from './pages/AllRooms.jsx'
+import RoomDetails from './pages/RoomDetails.jsx'
 
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
         <Routes>
           <Route path='/'element={<Home />}/>
           <Route path='/rooms'element={<AllRooms />}/>
+          <Route path='/rooms/:id'element={<RoomDetails />}/>
         </Routes>
       </div>
       <Footer />
