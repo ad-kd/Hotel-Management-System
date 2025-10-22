02e61c695d14d72e0d64720f49cd3d8141fff483
Adhithya K <12218adhithyaksnm@gmail.com>
2025-10-22 18:33:59 +0530

    owner section update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,9 @@ import RoomDetails from './pages/RoomDetails.jsx'
 import MyBookings from './pages/MyBookings.jsx'
 import HotelReg from './components/HotelReg.jsx'
 import Layout from './pages/hotelOwner/Layout.jsx'
+import Dashboard from './pages/hotelOwner/Dashboard.jsx'
+import AddRooms from './pages/hotelOwner/AddRoom.jsx'
+import ListRooms from './pages/hotelOwner/ListRoom.jsx'
 
 
 const App = () => {
@@ -25,7 +28,9 @@ const App = () => {
           <Route path='/rooms/:id'element={<RoomDetails />}/>
           <Route path='/my-bookings'element={<MyBookings />}/>
           <Route path='/owner' element={<Layout />}>
-
+            <Route index element={<Dashboard />} />
+            <Route path='add-room' element={<AddRooms />} />
+            <Route path='list-room' element={<ListRooms />} />
           </Route>
         </Routes>
       </div>
---
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Title from '../../components/Title'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Title align='left' font='outfit' title='Dashboard' subTitle='Monitor your 
+      room listings, track bookings and analyze revenue-all in one place. Stay updated
+      with real-time insights to ensure smooth operations.'/>
+
+      <div className='flex gap-4 my-8'>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Dashboard
