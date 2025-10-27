4f5a451e9dd393589432d77f6a6907696b5bf1a2
Adhithya K <12218adhithyaksnm@gmail.com>
2025-10-27 19:36:41 +0530

    update addrooms

--- client/src/pages/hotelOwner/AddRoom.jsx
+++ client/src/pages/hotelOwner/AddRoom.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddRoom = () => {
+  const [images, setImages] = useState({
+    1: null,
+    2: null,
+    3: null,
+    4: null,
+    5: null,
+  })
+
   return (
-    <div>AddRoom</div>
+    <div>
+
+    </div>
   )
 }
 
